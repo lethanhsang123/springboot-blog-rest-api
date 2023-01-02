b7120748018ac0f3f23b40ee9eee98fa2e7b1556
lethanhsang123 <lethanhsang14082001@gmail.com>
2023-01-02 23:33:58 +0700

    Push Project to Github

---
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -0,0 +1,67 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
+import com.springboot.blog.service.PostService;
+import com.springboot.blog.utils.AppConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // create blog post
+    @PostMapping("")
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    // get all posts Rest API
+    @GetMapping()
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+
+    ) {
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
+    }
+
+    // get post by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    // update post by id rest api
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto,
+                                              @PathVariable(name = "id") long id
+                                              ) {
+        PostDto postDtoResponse = postService.updatePost(postDto, id);
+
+        return new ResponseEntity<>(postDtoResponse, HttpStatus.OK);
+    }
+
+    // delete Post Rest API
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
+        postService.deletePostById(id);
+
+        return new ResponseEntity<>("Post entity deleted successfully", HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/springboot/blog/exception/BlogAPIException.java
@@ -0,0 +1,28 @@
+package com.springboot.blog.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * We throw this Exception whenever we write
+ * some business logic or validate request parameters
+ */
+public class BlogAPIException extends RuntimeException{
+
+    private HttpStatus status;
+    private String message;
+
+    public BlogAPIException(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public BlogAPIException(String message, HttpStatus status, String message1) {
+        super(message);
+        this.status = status;
+        this.message = message1;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
